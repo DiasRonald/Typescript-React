b989b38f37f09c0a1ac1a172de00ec266aff6cae
Ronald Dias <ronaldsilva850@gmail.com>
2024-08-07 00:18:11 -0300

    Parte 2 - Variáveis e Tipagens

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,60 @@
-function component() {
-    const element = document.getElementById('content');
-  
-    if (element) {
-      element.innerHTML = "<h1>Teste!</h1>";
-      element.innerHTML += "<h1>Teste!</h1>";
-      element.innerHTML += "<h1>Teste!</h1>";
-      element.innerHTML += "<h1>Teste!</h1>";
-      element.innerHTML += "<h1>Teste!</h1>";
-    } else {
-      console.error('Element with id "content" not found.');
-    }
-  
-    return element;
+const button = document.querySelector ('button[id="add"]') as HTMLButtonElement; 
+const accessRadio = document.getElementById ('accessRadio') as HTMLElement; 
+button.addEventListener('click', addEmployee);
+
+enum accessOptions {
+  administrator = "administrador",
+  manager = "gerente",
+  employee = "funcionário"
+}
+
+const accessOptionsValues = Object.values (accessOptions)
+
+accessOptionsValues.forEach ((value: string, i: number) => {
+  accessRadio.innerHTML += `
+  <div class="form-check">
+    <input class="form-check-input capitalLetter" type="radio" name="access id="accessRadio${i}" value="${value}">
+    <label class="form-check-label capitalLetter" for="accessRadio${i}">
+      ${value}
+    </label>
+  </div>
+  `;
+});
+
+function addEmployee (): void {
+  let content = document.getElementById ('content') as HTMLElement;
+  let fullName = document.querySelector ('#fullName') as HTMLInputElement;
+  let register = document.querySelector ('#register') as HTMLInputElement;
+  let admin = document.querySelector ('input[type="radio"]:checked') as HTMLInputElement;
+  let active = document.querySelector ('#active') as HTMLInputElement;
+
+  if (fullName && register && admin && active) {
+    content.innerHTML += createLine(
+      fullName.value,
+      register.value,
+      admin.value,
+      active.checked
+    );
   }
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    component();
-  });
-  
+}
+
+function createLine (fullName: string,
+                     NrRegister: string | number,
+                     admin: string,
+                     active: boolean): string {
+ return `
+    <div class="card mb-1">
+      <div class="card-header">
+        ${NrRegister}
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">${fullName}</h5>
+        <a href="#" class="btn ${active ? 'btn-success' : 'btn-danger'}">${active ? 'Ativo' : 'Inativo'}</a>
+      </div>
+      <div class="card-footer bg-transparent border-success">
+        Acesso: ${admin}
+      </div>
+    </div>`;
+  }
+
+
